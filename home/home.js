a20384dcd631477db056941c215eaed409c6c3c0
Rakshith Rao (rakrao) <rakrao@cisco.com>
2016-09-03 19:52:06 +0530

    basic loading of home page iwth backend

--- home/home.js
+++ home/home.js
@@ -7,147 +7,28 @@ angular.module('vidapp.home', ['ngRoute'])
             controller: 'homeCtrl'
         });
     }])
-    .controller('homeCtrl', function ($scope) {
-        var rows = [{
-            heading: 'Row Heading',
-            posts: [{
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016',  id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }]
-        }, {
-            heading: 'Row Heading',
-            posts: [{
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }]
-        }, {
-            heading: 'Row Heading',
-            posts: [{
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }]
-        }, {
-            heading: 'Row Heading',
-            posts: [{
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }]
-        },{
-            heading: 'Row Heading',
-            posts: [{
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }, {
-                img: 'http://img.youtube.com/vi/J3smwlrSiWY/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68',
-                heading: 'Nachde Ne Saare - Baar Baar Dekho | Sidharth M & Katrina K | Jasleen R | Harshdeep K, Siddharth MD',
-                views: 123456,
-                date: '20 Aug 2016', id: '_dCM_gWmsFM'
-            }]
-        }];
-        $scope.rows = rows;
+    .controller('homeCtrl', function ($scope, $http) {
+        $scope.loading = true;
+        var rows = [];
+        var row = {};
+        row.posts = [];
+        rows.push(row);
+        $http.jsonp('http://localhost:8080/playlist/PLzufeTFnhupy7sSmQdH5s3nUH8rwsua-U?callback=JSON_CALLBACK').then(function (res) {
+            var items = res.data.items;
+            for (var i in items) {
+                var item = items[i];
+                console.log(JSON.stringify(item));
+                row.posts.push({
+                    img: item.snippet.thumbnails.high.url,
+                    id: item.snippet.resourceId.videoId,
+                    heading: item.snippet.title,
+                    views: 2000,
+                    date: new Date(item.snippet.publishedAt)
+                });
+            }
+            $scope.loading = false;
+            $scope.rows = rows;
+        }, function (err) {
+            //TODO how to show error messages??
+        })
     });
