6dbcb397b8ba0e68b4b4157e611bbad6d3c5a720
Rakshith Rao (rakrao) <rakrao@cisco.com>
2016-09-03 11:54:36 +0530

    Deploy

--- boot.js
+++ boot.js
@@ -4,8 +4,8 @@ var http = require('http');
 var app = express();
 app.use('/', express.static('./'));
 
-http.createServer(app).listen(8080,
+http.createServer(app).listen(process.env.PORT,
     function () {
-        console.log("Express server listening on port 3000");
+        console.log("Express server listening on port %s", process.env.PORT);
     }
 );
