f365d47a9b44f667a0a99b51d63f46e2e896858c
Rakshith Rao (rakrao) <rakrao@cisco.com>
2016-09-05 14:27:32 +0530

    comment

--- boot.js
+++ boot.js
@@ -2,7 +2,8 @@ var express = require('express');
 var http = require('http');
 
 var app = express();
-app.use('/', express.static('./'));
+app.use('/', express.static('./app'));
+app.use('/ajax', express.static('./ajax'));
 
 http.createServer(app).listen(process.env.PORT,
     function () {
