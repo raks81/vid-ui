0b4a66b3d278cb6597708eb0eb5b91d03feb5e26
Rakshith Rao (rakrao) <rakrao@cisco.com>
2016-09-05 14:36:55 +0530

    changed context

--- boot.js
+++ boot.js
@@ -2,11 +2,13 @@ var express = require('express');
 var http = require('http');
 
 var app = express();
-app.use('/', express.static('./app'));
+app.use('/public', express.static('./app'));
 app.use('/ajax', express.static('./ajax'));
 
 http.createServer(app).listen(process.env.PORT,
     function () {
+        var port = process.env.PORT || '8080';
         console.log("Express server listening on port %s", process.env.PORT);
+        console.log("Open http://localhost:" + port + '/public/#/home to test');
     }
 );
